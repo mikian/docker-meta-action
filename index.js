787c96c374530b63cfb095c3754710497035bba4
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2021-03-01 19:52:50 +0100

    initial action

---
+++ index.js
@@ -0,0 +1,53 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function sha(context) {
+  if (context.payload && context.payload.pull_request) {
+    return context.payload.pull_request.head.sha;
+  } else {
+    return context.sha;
+  }
+}
+
+function metaLabels(context) {
+  return [
+    `org.opencontainers.image.created=${(new Date()).toISOString()}`,
+    `org.opencontainers.image.description=${context.payload.repository.description || ''}`,
+    `org.opencontainers.image.revision=${sha(context) || ''}`,
+    `org.opencontainers.image.source=${context.payload.repository.html_url || ''}`,
+    `org.opencontainers.image.title=${context.payload.repository.name || ''}`,
+    `org.opencontainers.image.url=${context.payload.repository.html_url || ''}`,
+  ];
+}
+
+function metaTags(image, context) {
+  var tags = [];
+
+  tags.push(`${image}:${sha(context)}`);
+
+  if (context.ref == `refs/heads/${context.payload.repository.default_branch}`) {
+    tags.push(`${image}:latest`)
+    tags.push(`${image}:release-${context.sha.substr(0, 7)}`)
+  } else {
+    tags.push(`${image}:dev-${context.sha.substr(0, 7)}`)
+  }
+
+  return tags;
+}
+
+try {
+  // Fetch inputs
+  const image = core.getInput('image');
+
+  // Initialize context
+  const labels = metaLabels(github.context);
+  const tags = metaTags(image, github.context);
+
+  console.log(`Labels:\n  ${labels.join(`\n  `)}`);
+  core.setOutput('labels', labels.join(`\n`));
+  console.log(`Tags:\n  ${tags.join(`\n  `)}`);
+  core.setOutput('tags', tags.join(`\n`));
+  core.setOutput('sha', sha(github.context));
+} catch (error) {
+  core.setFailed(error.message);
+}
